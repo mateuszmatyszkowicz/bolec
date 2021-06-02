7e22481bbeb89cc1bc32c44223e6b248259e6930
Paweł <pawel.gwozdecki@boldare.com>
2021-06-02 08:37:10 +0200

    not used parallel

--- src/demos/SaaSProductLandingPage.js
+++ src/demos/SaaSProductLandingPage.js
@@ -1,7 +1,6 @@
 import React from "react";
 import tw from "twin.macro";
 // import { css } from "styled-components/macro"; //eslint-disable-line
-import {TestId} from "consts/consts.js"
 import AnimationRevealPage from "helpers/AnimationRevealPage.js";
 import Hero from "components/hero/TwoColumnWithInput.js";
 import Features from "components/features/ThreeColWithSideImage.js";
